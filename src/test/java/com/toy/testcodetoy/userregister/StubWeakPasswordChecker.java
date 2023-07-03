426d7d5c0ff6fbc23bd42ddbad580e9826eae0dd
minjee <kimmj1106@naver.com>
2023-07-03 19:36:48 +0900

    [TEST] 회원가입 테스트2(TDD서적) - fake활용 레포지토리를 가짜 구현으로 사용

--- src/test/java/com/toy/testcodetoy/userregister/StubWeakPasswordChecker.java
+++ src/test/java/com/toy/testcodetoy/userregister/StubWeakPasswordChecker.java
@@ -2,7 +2,8 @@ package com.toy.testcodetoy.userregister;
 
 /**
  * 스텁(stub)?
- * - 구현을 단순한 것으로 대체한다. 테스트에 맞게 단순히 원하는 동작을 수행한다.
+ * - 구현을 단순한 것으로 대체한다.
+ * - 테스트에 맞게 '단순히 원하는 동작을 수행'한다.
  */
 
 public class StubWeakPasswordChecker implements WeakPasswordChecker{
--- src/test/java/com/toy/testcodetoy/userregister/UserRegisterTest.java
+++ src/test/java/com/toy/testcodetoy/userregister/UserRegisterTest.java
@@ -11,10 +11,11 @@ public class UserRegisterTest {
 
     private UserRegister userRegister;
     private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
 
     @BeforeEach
     void setUp() {
-        userRegister = new UserRegister(stubPasswordChecker);
+        userRegister = new UserRegister(stubPasswordChecker, fakeRepository);
     }
 
     @DisplayName("약한 암호면 가입 실패")
@@ -25,12 +26,35 @@ public class UserRegisterTest {
             - 실제 동작하는 구현은 필요하지 않다.
             - 약한 암호인지 여부를 알려주기만 하면 되므로 스텁 대역이면 충분하다.
          */
-        //스텁으로 하여금 암호 확인 효청이 오면 암호가 약하다고 응답하도록 설정
+        //스텁으로 하여금 암호 확인 요청이 오면 암호가 약하다고 응답하도록 설정
         //아직 구현 전이니까 그런 목적으로 작성한 코드임!
         stubPasswordChecker.setWeak(true);
 
         Assertions.assertThrows(WeakPasswordException.class, () -> {
             userRegister.register("id", "pw", "email");
         });
+    }
+
+    @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+    @Test
+    void dupIdExists() {
+        //이미 같은 ID가 존재하는 상황 만들기
+        fakeRepository.save(new User("id", "pw1", "email@email.com"));
+
+        Assertions.assertThrows(DupIdException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+
+    @DisplayName("같은 ID가 존재하지 않으면 가입 성공")
+    @Test
+    void noDupId_RegisterSuccess() {
+
+        userRegister.register("id", "pw", "email");
+
+        User saveUser = fakeRepository.findById("id"); //가입결과 확인
+
+        Assertions.assertEquals("id", saveUser.getId());
+        Assertions.assertEquals("email", saveUser.getEmail());
     }
 }
